57d2830b48e46298c0663fcd5cea95d9904c884f
mramankr97 <mramankr97>
2022-07-21 19:26:23 +0530

    aman

--- src/scripts/college.js
+++ src/scripts/college.js
@@ -120,3 +120,56 @@ document.getElementById('searchIcon').onclick = function() {
     document.getElementById('search').style.display = 'none';
     document.getElementById('clear').style.display = 'none';
   }
+
+
+
+  let url='https://newsapi.org/v2/top-headlines?country=in&apiKey=3fbeddfb8b474c858aded713acc19c9c'
+
+  async function getData()
+  {
+    let res=await fetch(url);
+    let data= await res.json();
+
+    console.log(data.articles);
+    display(data.articles)
+  }
+
+  getData()
+
+
+
+  function display(data)
+  {
+    let container=document.getElementById('indianews');
+
+    data.forEach((el)=>{
+        let image=document.createElement('img');
+        image.src=el.urlToImage;
+        image.style.width='250px';
+
+        let photo=document.createElement('div');
+        photo.append(image);
+
+
+        let title=document.createElement('h2');
+        title.innerText=el.title;
+
+        let desc=document.createElement('p');
+        desc.innerText=el.description
+        desc.style.marginLeft='-0.1px'
+
+        let div=document.createElement('div');
+        div.append(title,desc)
+        div.style.padding='5px 10px'
+
+        let box=document.createElement('div');
+        box.append(photo,div);
+        box.style.display='flex'
+        box.style.marginBottom='20px'
+
+        container.append(box);
+    })
+  }
+
+
+
